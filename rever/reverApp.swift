c7cf9e0b806b96460b09b3f90d11c16f14dd6658
Frank Schioppa <supergeek@me.com>
2025-12-06 08:31:00 -0500

    Initial Commit

---
+++ rever/reverApp.swift
@@ -0,0 +1,17 @@
+//
+//  reverApp.swift
+//  rever
+//
+//  Created by Frank Schioppa on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct reverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
